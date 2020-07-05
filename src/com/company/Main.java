07eb7e692d2d9d49d5b843b27c1df0747be53a82
Photon-139 <anshumanrahul06@gmail.com>
2020-07-05 16:32:59 +0530

    Quick Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String x="Hello",y="World";
+        System.out.println(x+" "+y);
+
+        System.out.printf("%s %s\n","Hello","World");
+
+        System.out.printf("%s %s\n",x,y);
+
+        String[] a={"H","e","l","l","o"," ","W","o","r","l","d"};
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+    }
+}
